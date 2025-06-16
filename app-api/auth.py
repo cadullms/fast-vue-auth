19491aac42ff77fdc9910053b64a3191f4d7f6d3
cadullms <cadullms@users.noreply.github.com>
2025-06-16 23:45:28 +0200

    multi-idp

--- app-api/auth.py
+++ app-api/auth.py
@@ -1,3 +1,4 @@
+import logging
 from urllib.parse import quote_plus, urlencode
 from authlib.integrations.starlette_client import OAuth
 from fastapi import APIRouter, HTTPException, Request, status
@@ -5,48 +6,82 @@ from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from config import AUTH_SETTINGS
 
-oauth = OAuth()
-oauth.register(
-    "auth0",
-    client_id=AUTH_SETTINGS.client_id,
-    client_secret=AUTH_SETTINGS.client_secret,
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f"https://{AUTH_SETTINGS.domain}/.well-known/openid-configuration",
-)
 
-# https://learn.microsoft.com/en-us/entra/identity-platform/v2-protocols-oidc#find-your-apps-openid-configuration-document-uri
+oauth = OAuth()
+for auth_option in AUTH_SETTINGS.options.values():
+    oauth.register(
+        auth_option.id,
+        client_id=auth_option.client_id,
+        client_secret=auth_option.client_secret,
+        server_metadata_url=f"https://{auth_option.domain}/.well-known/openid-configuration",
+        client_kwargs={
+            "scope": "openid profile email",
+        }
+    )
+
+_logger = logging.getLogger("uvicorn")
+
+
+async def _get_logout_endpoint(auth_option_id: str, request: Request) -> str:
+    auth_option = AUTH_SETTINGS.options[auth_option_id]
+    oauth_client = oauth.create_client(auth_option_id)
+    metadata = await oauth_client.load_server_metadata()
+    base_url = metadata.get("end_session_endpoint")
+    return_to = request.url_for("manage")
+    _logger.info(
+        f"Log out redirect. base_url: {base_url}, return_to: {return_to}, auth_option: {auth_option_id}, auth_option_type: {auth_option.type}")
+    if auth_option.type == "auth0":
+        return f"{base_url}?{urlencode(
+            {
+                "returnTo": return_to,
+                "client_id": auth_option.client_id,
+            },
+            quote_via=quote_plus,
+        )}"
+    if auth_option.type == "entra-id":
+        return f"{base_url}?{urlencode(
+            {
+                "post_logout_redirect_uri": return_to,
+            },
+            quote_via=quote_plus,
+        )}"
+    return base_url
 
 auth_router = APIRouter()
 
 
-@auth_router.get("/login")
-async def login(request: Request):
+@auth_router.get("/login/{auth_option_id}")
+async def login(auth_option_id: str, request: Request):
     if "id_token" not in request.session:
-        print("User not logged in, redirecting...")
-        return await oauth.auth0.authorize_redirect(
+        request.session["auth_option_id"] = auth_option_id
+        auth_client = oauth.create_client(auth_option_id)
+        if not auth_client:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Auth option {auth_option_id} not found",
+            )
+        _logger.info(f"User not logged in, redirecting with {auth_option_id}...")
+        return await auth_client.authorize_redirect(
             request, redirect_uri=request.url_for("callback")
         )
-    print("User already logged in...")
     return "user already logged in"
 
 
-@auth_router.get("/logout")
-async def logout(request: Request):
-    return_to = request.url_for("manage")
-    print(f"Logging out user, redirecting to {return_to}")
+@auth_router.get("/callback")
+async def callback(request: Request):
+    auth_option_id = request.session.get("auth_option_id")
+    token = await oauth.create_client(auth_option_id).authorize_access_token(request)
+    request.session["id_token"] = token["id_token"]
+    request.session["userinfo"] = token["userinfo"]
+    return RedirectResponse(url=request.url_for("manage"))
+
+
+@auth_router.get("/logout/{auth_option_id}")
+async def logout(auth_option_id: str, request: Request):
+    logout_url = await _get_logout_endpoint(auth_option_id, request)
+    _logger.info(f"logout_url: {logout_url}")
     response = RedirectResponse(
-        url=f"""https://{AUTH_SETTINGS.domain}/v2/logout?
-            {
-            urlencode(
-                {
-                    "returnTo": return_to,
-                    "client_id": AUTH_SETTINGS.client_id,
-                },
-                quote_via=quote_plus,
-            )
-        }"""
+        url=logout_url
     )
     request.session.clear()
     return response
@@ -64,19 +99,19 @@ def manage(request: Request):
             "id_token": request.session["id_token"]
             if "id_token" in request.session
             else None,
+            "auth_option_id": request.session["auth_option_id"]
+            if "auth_option_id" in request.session
+            else None,
+            "auth_options": AUTH_SETTINGS.options.values(),
+            "userinfo": request.session["userinfo"]
+            if "userinfo" in request.session
+            else None,
         },
     )
 
 
-@auth_router.get("/callback")
-async def callback(request: Request):
-    token = await oauth.auth0.authorize_access_token(request)
-    request.session["id_token"] = token["id_token"]
-    return RedirectResponse(url=request.url_for("manage"))
-
-
 def protected_endpoint(request: Request):
-    print("protected_endpoint called")
+    _logger.info("protected_endpoint called")
     if "id_token" not in request.session:
         # this will redirect people to the login after if they are not logged in
         raise HTTPException(
@@ -87,7 +122,7 @@ def protected_endpoint(request: Request):
 
 
 def protected_api(request: Request):
-    print("protected_api called")
+    _logger.info("protected_api called")
     if "id_token" not in request.session:
         # this will redirect people to the login after if they are not logged in
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
--- app-api/config.py
+++ app-api/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -8,10 +8,32 @@ load_dotenv(env_file_path)
 
 
 @dataclass
+class AuthOption:
+    id: str
+    name: str
+    client_id: str
+    client_secret: str
+    domain: str
+    type: str
+
+
+@dataclass
 class AuthSettings:
-    client_id: str = os.getenv("CLIENT_ID")
-    client_secret: str = os.getenv("CLIENT_SECRET")
-    domain: str = os.getenv("DOMAIN")
+    options: dict[str, AuthOption] | None = field(default_factory=dict)
 
 
 AUTH_SETTINGS = AuthSettings()
+
+auth_option_index = 0
+id = os.getenv(f"AUTH_OPTION_{auth_option_index}_ID")
+while id:
+    type = os.getenv(f"AUTH_OPTION_{auth_option_index}_TYPE")
+    name = os.getenv(f"AUTH_OPTION_{auth_option_index}_NAME")
+    client_id = os.getenv(f"AUTH_OPTION_{auth_option_index}_CLIENT_ID")
+    client_secret = os.getenv(
+        f"AUTH_OPTION_{auth_option_index}_CLIENT_SECRET")
+    domain = os.getenv(f"AUTH_OPTION_{auth_option_index}_DOMAIN")
+    AUTH_SETTINGS.options[id] = AuthOption(
+        id, name, client_id, client_secret, domain, type)
+    auth_option_index += 1
+    id = os.getenv(f"AUTH_OPTION_{auth_option_index}_ID")
